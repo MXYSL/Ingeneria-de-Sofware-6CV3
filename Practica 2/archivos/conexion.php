1058e017e0436d8e56ac80d893f67382a78dabfd
Mayra SL <164112862+MXYSL@users.noreply.github.com>
2025-03-28 19:02:24 -0600

    Update conexion.php

--- Practica 2/archivos/conexion.php
+++ Practica 2/archivos/conexion.php
@@ -1,9 +1,10 @@
+
 <?php
-// Configuración de la base de datos
-$servername = "localhost";
-$username = "root"; // 
-$password = ""; // 
-$dbname = "spring_auth";
+// Configuración de la base de datos usando variables de entorno de Docker
+$servername = getenv('MYSQL_HOST') ?: 'localhost';
+$username = getenv('MYSQL_USER') ?: 'root';
+$password = getenv('MYSQL_PASSWORD') ?: '';
+$dbname = getenv('MYSQL_DB') ?: 'spring_auth';
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
